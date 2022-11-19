596bc9f94b92a6d9d34d9f41533924848fa3547c
Mateusz Rybczynski <mateusz.rybczynski@icloud.com>
2022-11-19 18:53:23 +0100

    Creating adding function

--- lab4/js.js
+++ lab4/js.js
@@ -29,3 +29,38 @@ const clientData = [
 
 let db;
 let request = window.indexedDB.open("newDatabase", 1);
+
+request.onerror = function (event) {
+    console.log("Error. Database failed.");
+};
+
+request.onsuccess = function (event) {
+    db = request.result;
+    console.log("Success: The database is opened successfully");
+    drawTable();
+};
+
+request.onupgradeneeded = function (event) {
+    var db = event.target.result;
+    var objectStore = db.createObjectStore("client", {
+        autoIncrement: true,
+    });
+
+    objectStore.createIndex("name", "name", { unique: false });
+    objectStore.createIndex("lastName", "lastName", { unique: false });
+    objectStore.createIndex("age", "age", { unique: false });
+    objectStore.createIndex("email", "email", { unique: true });
+    objectStore.createIndex("pesel", "pesel", { unique: true });
+    objectStore.createIndex("address", "address", { unique: false });
+    objectStore.createIndex("phoneNumber", "phoneNumber", { unique: true });
+
+    for (var i in clientData) {
+        objectStore.add(clientData[i]);
+    }
+};
+
+function add(event) {
+    event.preventDefault();
+
+    var formElements = document.getElementById("addForm")
+;}
